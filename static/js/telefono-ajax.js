436121f193d444548362ece8a259940762bb6a07
Yohandri <yoha3001@gmail.com>
2015-04-09 13:40:54 -0430

    Estilos agregado part01
    no tomar en cuenta los cambios en views cliente y direccion

--- static/js/telefono-ajax.js
+++ static/js/telefono-ajax.js
@@ -1,13 +1,13 @@
-$ (Document).Ready(function(){
+//$ (Document).Ready(function(){
 
          // Código jQuery que se añade aquí.
-         $('#editar').click(function(){
-            alert('prueba' + $(this).attr("data-telid"))
-            var telid;
-            telid = $(this).attr("data-telid");
-            $.post('/Telefono/editar_telefono/', {id_telef: telid}, function(data){
-                $('.form-group').html(data);
-            });
-        });
+        // $('#editar').click(function(){
+         //   alert('prueba' + $(this).attr("data-telid"))
+           // var telid;
+            //telid = $(this).attr("data-telid");
+            //$.post('/Telefono/editar_telefono/', {id_telef: telid}, function(data){
+              //  $('.form-group').html(data);
+           // });
+        //});
 
-     });
+  //   });
